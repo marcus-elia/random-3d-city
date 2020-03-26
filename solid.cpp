b6024486615bff4670030dba85c88f5bed5304f2
marcus-elia <mselia@uvm.edu>
2020-03-26 08:47:40 -0400

    Added x,y,z widths to the Solid class

--- solid.cpp
+++ solid.cpp
@@ -2,11 +2,18 @@
 
 Solid::Solid() : DrawableObject()
 {
+    xWidth = 1;
+    yWidth = 1;
+    zWidth = 1;
     initializeCorners();
 }
 
-Solid::Solid(Point inputCenter, RGBAcolor inputColor) : DrawableObject(inputCenter, inputColor)
+Solid::Solid(Point inputCenter, RGBAcolor inputColor, double inputXWidth, double inputYWidth, double inputZWidth)
+: DrawableObject(inputCenter, inputColor)
 {
+    xWidth = inputXWidth;
+    yWidth = inputYWidth;
+    zWidth = inputZWidth;
     initializeCorners();
 }
 
@@ -14,9 +21,33 @@ std::vector<Point> Solid::getCorners() const
 {
     return corners;
 }
+double Solid::getXWidth() const
+{
+    return xWidth;
+}
+double Solid::getYWidth() const
+{
+    return yWidth;
+}
+double Solid::getZWidth() const
+{
+    return zWidth;
+}
 
 void Solid::setCorners(std::vector<Point> inputCorners)
 {
     corners = inputCorners;
+}
+void Solid::setXWidth(double inputXWidth)
+{
+    xWidth = inputXWidth;
+}
+void Solid::setYWidth(double inputYWidth)
+{
+    yWidth = inputYWidth;
+}
+void Solid::setZWidth(double inputZWidth)
+{
+    zWidth = inputZWidth;
 }
 
--- solid.h
+++ solid.h
@@ -10,19 +10,28 @@ class Solid : public DrawableObject
 {
 protected:
     std::vector<Point> corners;
+    double xWidth;
+    double yWidth;
+    double zWidth;
 public:
     // Constructors
     Solid();
-    Solid(Point inputCenter, RGBAcolor inputColor);
+    Solid(Point inputCenter, RGBAcolor inputColor, double inputXWidth, double inputYWidth, double inputZWidth);
 
     // Initialization
     virtual void initializeCorners() = 0;
 
-    // Getter
+    // Getters
     std::vector<Point> getCorners() const;
+    double getXWidth() const;
+    double getYWidth() const;
+    double getZWidth() const;
 
     // Setter
     void setCorners(std::vector<Point> inputCorners);
+    void setXWidth(double inputXWidth);
+    void setYWidth(double inputYWidth);
+    void setZWidth(double inputZWidth);
 };
 
 #endif //RANDOM_3D_CITY_SOLID_H
