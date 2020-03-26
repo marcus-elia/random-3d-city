f14bbddb6d420f1817e43439339d900bc86930be
marcus-elia <mselia@uvm.edu>
2020-03-26 13:28:48 -0400

    Added functions to get the camera location, and a Player private variable

--- gameManager.cpp
+++ gameManager.cpp
@@ -2,8 +2,8 @@
 
 GameManager::GameManager()
 {
-    solids.push_back(std::unique_ptr<RecPrism>(new RecPrism({0, 5, 0}, {0.2,0,1,1},
-            3,3, 10, {1,1,1,1})));
+    solids.push_back(std::unique_ptr<RecPrism>(new RecPrism({0, 50, 0}, {0.2,0,1,1},
+            30,100, 30, {1,1,1,1})));
 }
 
 void GameManager::draw() const
@@ -16,5 +16,28 @@ void GameManager::draw() const
 
 void GameManager::tick()
 {
+    player.tick();
+}
 
+Player GameManager::getPlayer() const
+{
+    return player;
+}
+std::vector<std::unique_ptr<Solid>> GameManager::getSolids() const
+{
+    return solids;
+}
+
+// Camera
+Vector3 GameManager::getCameraLocation() const
+{
+    return player.getLocation();
+}
+Vector3 GameManager::getCameraLookingAt() const
+{
+    return player.getLookingAt();
+}
+Vector3 GameManager::getCameraUp() const
+{
+    return player.getUp();
 }
--- gameManager.h
+++ gameManager.h
@@ -4,13 +4,25 @@
 #include <vector>
 #include <memory>
 #include "recPrism.h"
+#include "player.h"
 
 class GameManager
 {
 private:
+    Player player;
+
     std::vector<std::unique_ptr<Solid>> solids;
 public:
     GameManager();
+
+    // Getters
+    Player getPlayer() const;
+    std::vector<std::unique_ptr<Solid>> getSolids() const;
+
+    // Camera
+    Vector3 getCameraLocation() const;
+    Vector3 getCameraLookingAt() const;
+    Vector3 getCameraUp() const;
 
     void draw() const;
     void tick();
