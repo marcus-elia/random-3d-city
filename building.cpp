4046f437d475833251cfe3f28433ae0c7baa0361
marcus-elia <mselia@uvm.edu>
2020-03-31 20:33:28 -0400

    Moved the roof on top of the house, and added skyscrapers

--- building.cpp
+++ building.cpp
@@ -39,9 +39,16 @@ void Building::initializeSolids()
     {
         Point center = {(double)topLeft.x + sideLength/2, (double)height/2, (double)topLeft.z + sideLength/2};
         solids.push_back(std::make_shared<RecPrism>(RecPrism(center, color,
-                                                             sideLength, height, sideLength, edgeColor)));
-        solids.push_back(std::make_shared<TriPrism>(TriPrism(center, color,
+                                                             sideLength - 5, height, sideLength - 5, edgeColor)));
+        Point roofCenter = {center.x, center.y + 3*height/4, center.z};
+        solids.push_back(std::make_shared<TriPrism>(TriPrism(roofCenter, color,
                                                              sideLength, height/2, sideLength, edgeColor)));
+    }
+    else if(buildingType == Skyscraper)
+    {
+        Point center = {(double)topLeft.x + sideLength/2, (double)3*height/4, (double)topLeft.z + sideLength/2};
+        solids.push_back(std::make_shared<RecPrism>(RecPrism(center, color,
+                                                             sideLength, 3*height/2, sideLength, edgeColor)));
     }
 }
 
