c1a87abef312469dc3b788d8bc78294ccfeb4cb1
marcus-elia <mselia@uvm.edu>
2020-03-31 20:09:00 -0400

    Created the triPrism class, which can only have the base down

---
+++ triPrism.cpp
@@ -0,0 +1,110 @@
+#include "triPrism.h"
+
+TriPrism::TriPrism() : Solid(), MovableObject()
+{
+    initializeCorners();
+}
+TriPrism::TriPrism(Point inputCenter, RGBAcolor inputColor,
+                   double inputXWidth, double inputYWidth, double inputZWidth, RGBAcolor inputLineColor) :
+        Solid(inputCenter, inputColor, inputXWidth, inputYWidth, inputZWidth, inputLineColor),
+        MovableObject()
+{
+    initializeCorners();
+}
+
+void TriPrism::initializeCorners()
+{
+    corners.push_back({center.x + xWidth/2, center.y - yWidth/2, center.z + zWidth/2});
+    corners.push_back({center.x - xWidth/2, center.y - yWidth/2, center.z + zWidth/2});
+    corners.push_back({center.x, center.y + yWidth/2, center.z + zWidth/2});
+    corners.push_back({center.x - xWidth/2, center.y - yWidth/2, center.z - zWidth/2});
+    corners.push_back({center.x + xWidth/2, center.y - yWidth/2, center.z - zWidth/2});
+    corners.push_back({center.x, center.y + yWidth/2, center.z - zWidth/2});
+}
+
+
+void TriPrism::move(double deltaX, double deltaY, double deltaZ)
+{
+
+}
+
+void TriPrism::rotate(double thetaX, double thetaY, double thetaZ)
+{
+
+}
+
+void TriPrism::lookAt(Point &p)
+{
+
+}
+
+void TriPrism::draw() const
+{
+    drawLines();
+    drawFaces();
+}
+
+void TriPrism::drawLines() const
+{
+    glColor4f(lineColor.r, lineColor.g, lineColor.b, lineColor.a);
+    glBegin(GL_LINES);
+    drawPoint(corners[1]);
+    drawPoint(corners[0]);
+
+    drawPoint(corners[1]);
+    drawPoint(corners[2]);
+
+    drawPoint(corners[2]);
+    drawPoint(corners[0]);
+
+    drawPoint(corners[1]);
+    drawPoint(corners[4]);
+
+    drawPoint(corners[2]);
+    drawPoint(corners[5]);
+
+    drawPoint(corners[0]);
+    drawPoint(corners[3]);
+
+    drawPoint(corners[3]);
+    drawPoint(corners[4]);
+
+    drawPoint(corners[4]);
+    drawPoint(corners[5]);
+
+    drawPoint(corners[5]);
+    drawPoint(corners[3]);
+
+    glEnd();
+}
+
+void TriPrism::drawFaces() const
+{
+    glBegin(GL_QUADS);
+    glColor4f(color.r, color.g, color.b, color.a);
+
+    drawPoint(corners[0]);
+    drawPoint(corners[1]);
+    drawPoint(corners[2]);
+
+    drawPoint(corners[2]);
+    drawPoint(corners[1]);
+    drawPoint(corners[4]);
+    drawPoint(corners[5]);
+
+    drawPoint(corners[3]);
+    drawPoint(corners[5]);
+    drawPoint(corners[4]);
+
+    drawPoint(corners[5]);
+    drawPoint(corners[3]);
+    drawPoint(corners[0]);
+    drawPoint(corners[2]);
+
+    drawPoint(corners[1]);
+    drawPoint(corners[0]);
+    drawPoint(corners[3]);
+    drawPoint(corners[4]);
+
+    glEnd();
+}
---
+++ triPrism.h
@@ -0,0 +1,31 @@
+#ifndef RANDOM_3D_CITY_TRIPRISM_H
+#define RANDOM_3D_CITY_TRIPRISM_H
+
+#include "solid.h"
+#include "movableObject.h"
+
+// This makes an isosceles triangular prism whose
+// base is facing down
+
+class TriPrism : public Solid, public MovableObject
+{
+public:
+    TriPrism();
+    TriPrism(Point inputCenter, RGBAcolor inputColor,
+             double inputXWidth, double inputYWidth, double inputZWidth, RGBAcolor inputLineColor);
+
+    void initializeCorners();
+
+    void move(double deltaX, double deltaY, double deltaZ);
+
+    void rotate(double thetaX, double thetaY, double thetaZ);
+
+    void lookAt(Point &p);
+
+    void draw() const;
+    void drawLines() const;
+    void drawFaces() const;
+};
+
+
+#endif //RANDOM_3D_CITY_TRIPRISM_H
