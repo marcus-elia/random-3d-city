4d08096f50320aa538232f1d870fac0eb7118621
marcus-elia <mselia@uvm.edu>
2020-04-02 23:08:12 -0400

    Missiles are only shot when the button is released, not both

--- graphics.cpp
+++ graphics.cpp
@@ -175,7 +175,10 @@ void cursor(int x, int y)
 // state will be GLUT_UP or GLUT_DOWN
 void mouse(int button, int state, int x, int y)
 {
-    manager.reactToMouseClick();
+    if(state == GLUT_UP)
+    {
+        manager.reactToMouseClick();
+    }
     glutPostRedisplay();
 }
 
