7b32825349a3ba8535796ff90f334c758385613a
marcus-elia <mselia@uvm.edu>
2020-04-03 10:17:57 -0400

    Made missiles faster and smaller

--- gameManager.cpp
+++ gameManager.cpp
@@ -199,7 +199,7 @@ void GameManager::createMissile()
     Point velocity = {player.getLookingAt().x - location.x,
                       player.getLookingAt().y - location.y,
                       player.getLookingAt().z - location.z};
-    missiles.push_back(std::make_shared<Missile>(Missile(location, 5, velocity, 10)));
+    missiles.push_back(std::make_shared<Missile>(Missile(location, 10, velocity, 5)));
 }
 void GameManager::checkMissiles()
 {
